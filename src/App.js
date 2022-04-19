d439d8c383a9d722fe75dadb8d64e77307660eed
Temple <Templevoke@gmail.com>
2022-04-19 17:14:16 +0100

    19th april 2022

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import './App.css';
 
 function App() {
+  const [state,setState] = useState({count:4,theme:"blue"})
+  console.log(state)
+  const {count , theme}= state;
+
+  function increamentstate(){
+    setState((previou)=>{
+      return { ...previou ,count:count + 1
+      }
+    })
+  }
+
+  function decreamentstate(){
+    setState((pav)=>{
+      return {...pav ,count:count -1 }
+    }
+    )
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
+    <button onClick={increamentstate} style={{type:'button',background:"blue",padding:'20px'}}>+</button>
+    <span style={{padding:'20px', background:"black", color:'white',fontSize:'25px'}}>{count}</span>
+    <span style={{padding:'20px', background:"black", color:'white',fontSize:'25px'}}>{theme}</span>
+    <button onClick={decreamentstate} style={{type:'button',background:"green",padding:'20px'}}>-</button>
     </div>
   );
 }
